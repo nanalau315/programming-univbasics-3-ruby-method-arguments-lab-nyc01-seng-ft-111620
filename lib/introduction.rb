93edd28b745686ce403946d9136fc9115134f7f8
Wing Nam Lau <nanalau315@hotmail.com>
2020-10-29 23:12:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@
 # {introduction("Josh")}.to output("Hi, my name is Josh.\n")
 
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 # introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n")
