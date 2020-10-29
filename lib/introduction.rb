9aa5de5c465f972d777dbb91ace3d9fe3082d3c6
Wing Nam Lau <nanalau315@hotmail.com>
2020-10-29 23:08:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,11 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+{introduction("Josh")}.to output("Hi, my name is Josh.\n")
 
-
-
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
 
 
 
